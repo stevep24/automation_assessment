07453860ecbe244fd2ade357fc0d8beff517f87c
stevep <stevep_pan@yahoo.com>
2024-12-20 18:24:23 +0200

    apply the .env encryption

--- AutomationExercise1/configure_test.py
+++ AutomationExercise1/configure_test.py
@@ -5,16 +5,15 @@ from dotenv import load_dotenv
 import os
 
 
-
 @pytest.fixture
 def driver():
-    #load_dotenv()
+    load_dotenv()
     website = "https://opaponline.opap.gr/en/"
-    path = "C:\\Users\\STEVE\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe"
-    #website=os.getenv("website")
-    #path=os.getenv("path")
-    print(website,path)
-    service = Service(executable_path=path)
+    chromedriver_path = "C:\\Users\\STEVE\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe"
+    if not website or not chromedriver_path:
+        raise ValueError("Environment variables 'website' or 'path' not loaded correctly.")
+
+    service = Service(executable_path=chromedriver_path)
     driver = webdriver.Chrome(service=service)
     driver.get(website)
     yield driver
